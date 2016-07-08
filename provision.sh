9610b1efb29d07097d7d1a54be740d570b59e5cc
BlueHatbRit <myalt94@gmail.com>
2016-07-08 12:56:33 +0100

    Ignore RVM and use a single static ruby install

--- provision.sh
+++ provision.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+
 RUBY_VERSION="2.3.1"
 
 sudo apt-get -y update
 
-if ! rvm list rubies ruby | grep ruby-${RUBY_VERSION}; then
-  rvm install ${RUBY_VERSION}
+# Get ruby-build
+cd ~/
+git clone https://github.com/sstephenson/ruby-build.git
+
+# Install Ruby
+~/ruby-build/bin/ruby-build ${RUBY_VERSION} ~/${RUBY_VERSION}
+
+# Export Ruby to PATH, ensuring it's at the start so it doesn't get confused with the system default
+echo 'export PATH="$HOME/2.3.1/bin:$PATH" # Add Ruby to path' >> ~/.bashrc
+
+source ~/.bashrc # Reload the bashrc
+
+# Install and update RubyGem
+~/2.3.1/bin/gem update --system # Grab the latest, we're not fussy
+
+# Install bundler if we don't already have it
+if ! sudo gem list bundler -i > /dev/null; then
+  sudo gem install bundler
 fi
-
-# for some reason this isn't working...
-#rvm use ${RUBY_VERSION}
-
-#gem install bundler
